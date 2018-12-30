a28e18a2c6477369bf20f5985a55891a69d703f7
Uwe Rosner <oscottmusic@posteo.de>
2018-12-30 11:46:47 +0100

    Implemented the loading / displaying of a test image

--- GamePhysicsSDLMagic/01-HelloSDL.cpp
+++ GamePhysicsSDLMagic/01-HelloSDL.cpp
@@ -13,43 +13,47 @@ const int SCREEN_HEIGHT = 480;
 
 int main(int argc, char* args[])
 {
-  //The window we'll be rendering to
-  SDL_Window* window = NULL;
-
-  //The surface contained by the window
-  SDL_Surface* screenSurface = NULL;
-
   //Initialize SDL
   if (SDL_Init(SDL_INIT_VIDEO) < 0)
   {
     printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+    return 20;
   }
-  else
+
+  //Create window
+  SDL_Window* pWindow = SDL_CreateWindow("SDL Tutorial", 
+    SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, 
+    SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+  if (pWindow == NULL)
   {
-    //Create window
-    window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    if (window == NULL)
-    {
-      printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-    }
-    else
-    {
-      //Get window surface
-      screenSurface = SDL_GetWindowSurface(window);
-
-      //Fill the surface white
-      SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-
-      //Update the surface
-      SDL_UpdateWindowSurface(window);
-
-      //Wait two seconds
-      SDL_Delay(2000);
-    }
+    printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+    return 20;
   }
+
+  //Get window surface
+  SDL_Surface* pScreenSurface = SDL_GetWindowSurface(pWindow);
+
+  //Fill the surface white
+  SDL_Surface* pPictureSurface = SDL_LoadBMP("Images/Hello_world.bmp");
+  if (pPictureSurface == NULL)
+  {
+    printf("Unable to load the image: %s\n", SDL_GetError());
+    return 20;
+  }
+
+  SDL_BlitSurface(pPictureSurface, NULL, pScreenSurface, NULL);
+
+  //Update the surface
+  SDL_UpdateWindowSurface(pWindow);
+
+  //Wait two seconds
+  SDL_Delay(2000);
+
+  SDL_FreeSurface(pPictureSurface);
 
   //Destroy window
-  SDL_DestroyWindow(window);
+  SDL_DestroyWindow(pWindow);
 
   //Quit SDL subsystems
   SDL_Quit();
